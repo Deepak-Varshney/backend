c21a61bbe809c73b58e6e74db643171d2623ba80
Deepak Varshney <deepakvarshney.com@gmail.com>
2026-06-28 14:27:59 +0530

    prisma adapter build fix

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -1,15 +1,3 @@
-import "dotenv/config";
-import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 import { PrismaClient } from "@prisma/client";
 
-const adapter = new PrismaMariaDb({
-  host: process.env.DB_HOST!,
-  port: Number(process.env.DB_PORT!),
-  user: process.env.DB_USER!,
-  password: process.env.DB_PASSWORD!,
-  database: process.env.DB_NAME!,
-  ssl: { rejectUnauthorized: false },
-  connectionLimit: 10,
-});
-
-export const prisma = new PrismaClient({ adapter });
+export const prisma = new PrismaClient();
